e19f2b805b8f6f814f9f2c34b313c2dbe163c00e
ledoctah <matheusferr2015@gmail.com>
2021-03-22 20:33:27 -0300

    Refactored header + input + modal

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,9 +8,16 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconType } from 'react-icons/lib';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputComponentsProps {
+  name: string,
+  icon: IconType;
+  children: React.ReactNode;
+}
+
+export const Input = ({ name, icon: Icon, ...rest }: IInputComponentsProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +56,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
